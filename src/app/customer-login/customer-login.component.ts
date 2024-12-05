7a59cbc96efcb9e05c58491102028bf938b5d896
wolech <wojciech.lech@hyland.com>
2024-12-05 15:47:27 +0100

    crud-app

---
+++ src/app/customer-login/customer-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-login',
+  imports: [],
+  templateUrl: './customer-login.component.html',
+  styleUrl: './customer-login.component.css'
+})
+export class CustomerLoginComponent {
+
+}
---
+++ src/app/customer-update/customer-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-update',
+  imports: [],
+  templateUrl: './customer-update.component.html',
+  styleUrl: './customer-update.component.css'
+})
+export class CustomerUpdateComponent {
+
+}
